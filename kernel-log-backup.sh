6971f5bef33a95efb35cd2e41bd8878532829293
xuanthudao <57358957+xuanthudao@users.noreply.github.com>
2020-04-06 02:13:41 -0400

    Update kernel-log-backup.sh

--- kernel-log-backup.sh
+++ kernel-log-backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if 
-	/var/log/kern.log> kern.log.backup 2>&1
-then
-	/var/log/kern.log>/dev/null 2>&1
+
+if [-s /var/log/kern.log]
+	then /var/log/kern.log> kern.log.backup && /var/log/kern.log>/dev/null 2>&1
+else
+	exit 1
 fi
 exit 0
